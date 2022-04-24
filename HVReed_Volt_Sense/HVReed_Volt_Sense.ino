ec19676606faf2b5bb6bdce26e713a551b914be4
unknown <lynaries@gmail.com>
2022-04-24 18:04:56 +0800

    Updated according to the proposed flowchart

--- HVReed_Volt_Sense/HVReed_Volt_Sense.ino
+++ HVReed_Volt_Sense/HVReed_Volt_Sense.ino
@@ -1,8 +1,9 @@
 // HV Reed Voltage Sensor Relay System
-// Rev 1.1 (19/04/2022)
+// Rev 1.1 (24/04/2022)
 // - Maxtrax
 
 #include <Wire.h>
+#include <Scheduler.h>
 #include <Adafruit_MCP23008.h>
 #include <DTIOI2CtoParallelConverter.h>
 #include <ArduinoRS485.h>
@@ -10,6 +11,16 @@
 #define NOP __asm__("nop\n\t") //"nop" executes in one machine cycle (at 16 MHz) yielding a 62.5 ns delay
 
 const char * app_ver = "v1.1";
+
+//Master commands
+const char * ACK_STR = "ACK";
+const char * NACK_STR = "NACK";
+const char * QUERY_BRD_STR = "QBS";
+const char * QUERY_CH_STR = "QCS";
+const char * END_STR = "END";
+const char * DBG_STR = "HDB";
+const char DELIM = ',';
+const byte CMD_LEN = 3;
 
 const byte LED_PIN = 16;
 
@@ -28,7 +39,7 @@ const byte LEVEL_SHIFTER_OE = 3;
 
 const byte TOTAL_IOEXP_PORTS = 8;
 
-const int MAX_BUFFERED_CMD = 16;
+const int MAX_BUFFERED_CMD = 32;
 
 const byte LF = 0x0A;
 
@@ -62,8 +73,12 @@ DTIOI2CtoParallelConverter ioExp4_U5(0x77);  //PCA9539 I/O Expander (with A1 = 1
 
 Adafruit_MCP23008 ioExp_MCP;
 
-byte board_ID = 0; //own board ID
+byte board_ID = 0;
 byte check_bit = 0; //use bits to represent the interrupt triggerd expander
+bool first_delim_found = false;
+bool second_delim_found = false;
+int delim_idx = 0;
+int end_idx = 0;
 int cmd_idx = 0;
 char cmd_str[MAX_BUFFERED_CMD] = {};
 String result = "";
@@ -121,42 +136,42 @@ void checkIOExpanderPins(port_data_t *p_expander)
         byte pin_count = 0;
         if( (pin_count++ < p_expander->max_pins) && (HIGH == p_expander->p_expander_port->pins.pin_0) )
         {
-            result += (String(1 + p_expander->pin_offset) + String(","));
+            result += (String(1 + p_expander->pin_offset) + String(DELIM));
         }
         
         if( (pin_count++ < p_expander->max_pins) && (HIGH == p_expander->p_expander_port->pins.pin_1) )
         {
-            result += (String(2 + p_expander->pin_offset) + String(","));
+            result += (String(2 + p_expander->pin_offset) + String(DELIM));
         }
         
         if( (pin_count++ < p_expander->max_pins) && (HIGH == p_expander->p_expander_port->pins.pin_2) )
         {
-            result += (String(3 + p_expander->pin_offset) + String(","));
+            result += (String(3 + p_expander->pin_offset) + String(DELIM));
         }
         
         if( (pin_count++ < p_expander->max_pins) && (HIGH == p_expander->p_expander_port->pins.pin_3) )
         {
-            result += (String(4 + p_expander->pin_offset) + String(","));
+            result += (String(4 + p_expander->pin_offset) + String(DELIM));
         }
         
         if( (pin_count++ < p_expander->max_pins) && (HIGH == p_expander->p_expander_port->pins.pin_4) )
         {
-            result += (String(5 + p_expander->pin_offset) + String(","));
+            result += (String(5 + p_expander->pin_offset) + String(DELIM));
         }
         
         if( (pin_count++ < p_expander->max_pins) && (HIGH == p_expander->p_expander_port->pins.pin_5) )
         {
-            result += (String(6 + p_expander->pin_offset) + String(","));
+            result += (String(6 + p_expander->pin_offset) + String(DELIM));
         }
         
         if( (pin_count++ < p_expander->max_pins) && (HIGH == p_expander->p_expander_port->pins.pin_6) )
         {
-            result += (String(7 + p_expander->pin_offset) + String(","));
+            result += (String(7 + p_expander->pin_offset) + String(DELIM));
         }
         
         if( (pin_count++ < p_expander->max_pins) && (HIGH == p_expander->p_expander_port->pins.pin_7) )
         {
-            result += (String(8 + p_expander->pin_offset) + String(","));
+            result += (String(8 + p_expander->pin_offset) + String(DELIM));
         }
     }
 }
@@ -187,6 +202,35 @@ void ioExp4InterruptHandler()
     ioExp4_U5.digitalReadPort0(ioExp4_port0.port);
     ioExp4_U5.digitalReadPort1(ioExp4_port1.port);
     check_bit |= 0xC0;
+}
+
+void updateResult()
+{
+    //interrupt triggered process the read data here
+    while(0 != check_bit)
+    {
+        byte port_mask = 0;
+        do
+        {
+            //loop for which expander interrupt triggered
+            if(check_bit & (1 << port_mask))
+            {
+                checkIOExpanderPins(&expander_mapping[port_mask]);
+                check_bit &= ~(1 << port_mask); //clear bit after handle
+            }
+        }while( (++port_mask < TOTAL_IOEXP_PORTS) && (0 != check_bit) );
+    }
+    
+    yield(); //yield to pass control to other tasks
+}
+
+void resetBuffer()
+{
+    delim_idx = 0;
+    end_idx = 0;
+    first_delim_found = false;
+    second_delim_found = false;
+    memset(cmd_str, 0, MAX_BUFFERED_CMD);
 }
 
 void setup()
@@ -249,40 +293,75 @@ void setup()
     attachInterrupt(digitalPinToInterrupt(IOEXP_INT4_PIN), ioExp4InterruptHandler, CHANGE);
     
     digitalWrite(LED_PIN, LOW);
+    
+    Scheduler.startLoop(updateResult); //update result from interrupt
 }
 
 void loop()
 {
-    //interrupt triggered process the read data here
-    while(0 != check_bit)
-    {
-        byte port_mask = 0;
-        do
-        {
-            //loop for which expander interrupt triggered
-            if(check_bit & (1 << port_mask))
-            {
-                checkIOExpanderPins(&expander_mapping[port_mask]);
-                check_bit &= ~(1 << port_mask); //clear bit after handle
-            }
-        }while( (++port_mask < TOTAL_IOEXP_PORTS) && (0 != check_bit) );
-    }
-    
     //get request from Master
-    if (RS485.available())
+    while (RS485.available())
     {
         cmd_str[cmd_idx] = RS485.read();
-        if (cmd_str[cmd_idx] == LF)
+        
+        if (cmd_str[cmd_idx] == DELIM)
         {
-            cmd_str[cmd_idx-1] = 0;
-            Serial.println(cmd_str); //the request command from Master
-            cmd_idx = -1;
-            
-            String reply = (String(board_ID) + String(",") + result);
-            reply.setCharAt(reply.length(), 'S'); //use 'S' for END char
-            sendRS485Data(const_cast<char *>(reply.c_str()));
-            result = ""; //clear the result after sending the reply
+            //1st delimiter found
+            if (!first_delim_found)
+            {
+                delim_idx = cmd_idx;
+                first_delim_found = true;
+            }
+            //2nd delimiter found
+            else
+            {
+                end_idx = cmd_idx;
+                second_delim_found = true;
+            }
         }
+        
+        //2nd delimiter found and at least 3 bytes arrived after 2nd delimiter
+        if ( (second_delim_found) && (cmd_idx >= end_idx + CMD_LEN) )
+        {
+            //check for END
+            if (strncmp(&cmd_str[end_idx+1], END_STR, CMD_LEN) == 0)
+            {
+                //check for matching board ID
+                int tmp_board_id = atoi(&cmd_str[delim_idx+1]);
+                if (tmp_board_id == board_ID)
+                {
+                    //parse first 3 bytes of data for request type
+                    if (strncmp(cmd_str, QUERY_BRD_STR, CMD_LEN) == 0)
+                    {
+                        sendRS485Data(const_cast<char *>(ACK_STR));
+                    }
+                    else if (strncmp(cmd_str, QUERY_CH_STR, CMD_LEN) == 0)
+                    {
+                        String reply = (String(board_ID) + String(DELIM) + result + String(END_STR));
+                        sendRS485Data(const_cast<char *>(reply.c_str()));
+                        result = ""; //clear the result after sending the reply
+                    }
+                    else if (strncmp(cmd_str, DBG_STR, CMD_LEN) == 0)
+                    {
+                        //TODO: HW debug handling
+                    }
+                    else //unknown command send NACK
+                    {
+                        sendRS485Data(const_cast<char *>(NACK_STR));
+                    }
+                }
+                
+                resetBuffer();
+                cmd_idx = -1;
+            }
+        }
+
         cmd_idx++;
+        
+        if(cmd_idx >= MAX_BUFFERED_CMD) //buffer overflowing!
+        {
+            resetBuffer();
+            cmd_idx = 0;
+        }
     }
 }
